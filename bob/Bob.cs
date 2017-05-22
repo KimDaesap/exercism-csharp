eac5da2887c0752d708ccdfd2dd6ea67a7b0e6e0
Choi sangmin <sapzil78jw@gmail.com>
2017-05-22 14:32:09 +0900

    bob improve

--- bob/Bob.cs
+++ bob/Bob.cs
@@ -4,10 +4,17 @@ public static class Bob
 {
     public static string Hey(string statement)
     {
-        if (statement.ToUpper().Equals(statement) &&
-         !statement.ToLower().Equals(statement)) return  "Whoa, chill out!";
-        else if (statement.Trim().EndsWith("?")) return "Sure.";
-        else if (string.IsNullOrEmpty(statement.Trim())) return "Fine. Be that way!";
-        else return "Whatever.";
+        if (statement.ToUpper().Equals(statement) && !statement.ToLower().Equals(statement)) {
+             return  "Whoa, chill out!";
+        }
+        else if (statement.Trim().EndsWith("?")) {
+            return "Sure.";
+        }
+        else if (string.IsNullOrEmpty(statement.Trim())) {
+            return "Fine. Be that way!";
+        }
+        else{
+            return "Whatever.";
+        }
     }
 }
